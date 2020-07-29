49dc782e4d53d7e293138169bee0e4f40cb8dd36
tadasbytautas <tadasbytautas1@gmail.com>
2020-07-29 09:18:46 +0000

    changes on stack_deploy file

--- scripts/stack_deploy.sh
+++ scripts/stack_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$(docker stack ls 2> /dev/null)" == "" ]]; then
+if [[ "$(docker stack services SFIA2 2> /dev/null)" == "" ]]; then
     docker stack deploy --compose-file docker-compose.yml SFIA2
 else
     docker stack ls
