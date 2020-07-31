ac943a6101c21bb578e2c79a215a42d90656170f
tadasbytautas <tadasbytautas1@gmail.com>
2020-07-31 11:09:34 +0000

    adding database

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -1,12 +1,23 @@
 from application import app
 from flask import render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import requests
 import random
 
+class nickname_gen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)    
+    response = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return ''.join([
+            'ID: ', str(self.id), '\r\n',
+            'response: ', str(self.response)
+        ])
 
 @app.route('/', methods=['GET'])
 def home():
     response = requests.get('http://service_4:5003/randomword')
     print(response)
     sentence = response.text
-    return render_template('index.html', sentence = sentence, title = 'Home')
+    post_data = nickname_gen.query.all()    
+    return render_template('index.html', sentence = sentence, title = 'Home', sentence=post_data)
