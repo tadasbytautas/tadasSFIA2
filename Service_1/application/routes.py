1fe9841a3f8894291cf1a262ba0f8ec4f368c33a
tadasbytautas <tadasbytautas1@gmail.com>
2020-07-31 13:18:06 +0000

    testing

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -33,7 +33,7 @@ def home():
     response = requests.get('http://service_4:5003/randomword')
     print(response)
     sentence = response.text
-    post_data = nickname_gen.query.all()
+    # post_data = nickname_gen.query.all()
     db.session.add()
     db.session.commit()   
-    return render_template('index.html', sentence = sentence, title = 'Home', post=post_data)
+    return render_template('index.html', sentence = sentence, title = 'Home')
