2a02a811472f21ed08d92d4301b24980d8f9ce5c
tadasbytautas <tadasbytautas1@gmail.com>
2020-07-31 12:29:16 +0000

    more changes to db

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -3,6 +3,20 @@ from flask import render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import requests
 import random
+import os 
+
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
+                                        environ.get('MYSQL_USER') + \
+                                        ':' + \
+                                        environ.get('MYSQL_PASSWORD') + \
+                                        '@' + \
+                                        environ.get('MYSQL_HOST') + \
+                                        ':' + \
+                                        environ.get('MYSQL_PORT') + \
+                                        '/' + \
+                                        environ.get('MYSQL_DB_NAME')
 
 class nickname_gen(db.Model):
     id = db.Column(db.Integer, primary_key=True)    
@@ -20,4 +34,4 @@ def home():
     print(response)
     sentence = response.text
     post_data = nickname_gen.query.all()    
-    return render_template('index.html', sentence = sentence, title = 'Home', sentence=post_data)
+    return render_template('index.html', sentence = sentence, title = 'Home', post=post_data)
