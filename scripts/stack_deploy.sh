589b3f604da4534f19da8802c0c8ac1df6edbe8e
tadasbytautas <tadasbytautas1@gmail.com>
2020-08-04 09:57:46 +0000

    test

--- scripts/stack_deploy.sh
+++ scripts/stack_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /var/lib/jenkins/.env
+# source /var/lib/jenkins/.env
 
 if [[ "$(docker stack services SFIA2 2> /dev/null)" == "" ]]; then
     docker stack deploy --compose-file docker-compose.yml SFIA2
