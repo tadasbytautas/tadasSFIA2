a24c030eb917b0f0a0cd736fefe55ffed840220f
tadasbytautas <tadasbytautas1@gmail.com>
2020-08-05 10:03:14 +0000

    more changes to build

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -20,9 +20,4 @@ docker build -t tbytautas/service_4:latest ./Service_4
 #Pushes new built image to docker hub
 docker push tbytautas/service_4
 
-#Build clean version of nginx image
-docker build -t tbytautas/nginx:latest ./NGINX
-#Pushes new built image to docker hub
-docker push tbytautas/nginx
-
  
