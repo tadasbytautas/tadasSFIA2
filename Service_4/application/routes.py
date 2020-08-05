e1b4cc97d62729bf68c450ea49b6bafeffc48db5
tadasbytautas <tadasbytautas1@gmail.com>
2020-08-05 13:22:17 +0000

    test

--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -4,7 +4,7 @@ import requests
 
 @app.route('/randomword', methods=['GET'])
 def sentence():
-    beginning = requests.get('http://localhost:5001/randomphrase')
-    ending = requests.get('http://localhost:5002/randomphrase')
+    beginning = requests.get('http://service_2:5001/randomphrase')
+    ending = requests.get('http://service_3:5002/randomphrase')
     response = beginning.text + " " + ending.text
     return response
