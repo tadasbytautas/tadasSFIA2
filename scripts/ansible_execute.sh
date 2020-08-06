fe92d10d09ce5d18943cd3af34c751006c68fa5e
tadasbytautas <tadasbytautas1@gmail.com>
2020-08-06 09:51:46 +0000

    added ansible to build

--- scripts/ansible_execute.sh
+++ scripts/ansible_execute.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-sudo apt install ansible -y
+# make sure ~/.local/bin exists and is on your PATH
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+## install ansible with pip
+pip install --user ansible
+# check that ansible has been installed
+ansible --version
+
+#run ansible-playbook to install dependencies
 ansible_playbook -i inventory.cfg playbook.yml
+
