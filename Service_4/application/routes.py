d8b4a1cd8ad0e5ef5ebb566364749d1ca20e14b5
tadasbytautas <tadasbytautas1@gmail.com>
2020-08-10 09:47:44 +0000

    testing service 4

--- Service_4/application/routes.py
+++ Service_4/application/routes.py
@@ -7,5 +7,5 @@ def sentence():
     beginning = requests.get('http://service_2:5001/randomphrase')
     ending = requests.get('http://service_3:5002/randomphrase')
     # response = beginning.text + " " + ending.text 
-    response = beginning.text + " --change on service 4-- " + ending.text 
+    response = beginning.text + " (Service 4 change) " + ending.text 
     return response
