f868f8b26f9fb4802f0fdbd84b05691bde56a406
tadasbytautas <tadasbytautas1@gmail.com>
2020-08-10 10:33:21 +0000

    more testing

--- Service_3/application/routes.py
+++ Service_3/application/routes.py
@@ -5,7 +5,7 @@ import random
 @app.route('/randomphrase', methods=['GET'])
 def ending():
 
-	# list = ['Cole','Ferenczy','Otonashi','Munster','Tepesh','Corvinus','Count','Petrova','Knight']
-	list = ['change on service 2a','change on service 2b','change on service 2c','change on service 2d','change on service 2e','change on service 2f','change on service 2g','change on service 2k','change on service 2l']
+	list = ['Cole','Ferenczy','Otonashi','Munster','Tepesh','Corvinus','Count','Petrova','Knight']
+	# list = ['change on service 3a','change on service 3b','change on service 3c','change on service 3d','change on service 3e','change on service 3f','change on service 3g','change on service 3k','change on service 3l']
 	
 	return list[random.randrange(8)]
