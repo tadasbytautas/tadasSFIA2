e7f8051b4584cb277c23362c3bd59d8e5cbeae41
tadasbytautas <tadasbytautas1@gmail.com>
2020-08-10 10:11:57 +0000

    slight changes

--- Service_2/application/routes.py
+++ Service_2/application/routes.py
@@ -5,7 +5,7 @@ import random
 @app.route('/randomphrase', methods=['GET'])
 def beginning():
 
-	list = ['Brainsucker','Lionel','Grabber','Riley','Crawler','Matterslurper','Abomination','Lurker','Adolf']
-	# list = ['change on service 1a','change on service 1b','change on service 1c','change on service 1d','change on service 1e','change on service 1f','change on service 1g','change on service 1k','change on service 1l']
+	# list = ['Brainsucker','Lionel','Grabber','Riley','Crawler','Matterslurper','Abomination','Lurker','Adolf']
+	list = ['(change on service 1a)','(change on service 1b)','(change on service 1c)','(change on service 1d)','(change on service 1e)','(change on service 1f)','(change on service 1g)','(change on service 1k)','(change on service 1l)']
 	
 	return list[random.randrange(8)]
